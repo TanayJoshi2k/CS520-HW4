f7673ecad9668e1c6d50613ba29682d1450bd9a3
Tanay Joshi <tanaypjoshi@gmail.com>
2023-12-03 13:17:55 -0500

    Added observer design pattern.

--- expense_tracker/src/model/ExpenseTrackerModel.java
+++ expense_tracker/src/model/ExpenseTrackerModel.java
@@ -2,63 +2,70 @@ package model;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ExpenseTrackerModel {
 
-  //encapsulation - data integrity
+  // encapsulation - data integrity
   private List<Transaction> transactions;
   private List<Integer> matchedFilterIndices;
+  protected Set<ExpenseTrackerModelListener> listeners = new HashSet<>();
+  // This is applying the Observer design pattern.
+  // Specifically, this is the Observable class.
 
-  // This is applying the Observer design pattern.                          
-  // Specifically, this is the Observable class. 
-    
   public ExpenseTrackerModel() {
     transactions = new ArrayList<Transaction>();
     matchedFilterIndices = new ArrayList<Integer>();
   }
 
   public void addTransaction(Transaction t) {
-    // Perform input validation to guarantee that all transactions added are non-null.
+    // Perform input validation to guarantee that all transactions added are
+    // non-null.
     if (t == null) {
       throw new IllegalArgumentException("The new transaction must be non-null.");
     }
     transactions.add(t);
+    stateChanged();
     // The previous filter is no longer valid.
     matchedFilterIndices.clear();
   }
 
   public void removeTransaction(Transaction t) {
     transactions.remove(t);
+    stateChanged();
     // The previous filter is no longer valid.
     matchedFilterIndices.clear();
   }
 
   public List<Transaction> getTransactions() {
-    //encapsulation - data integrity
+    // encapsulation - data integrity
     return Collections.unmodifiableList(new ArrayList<>(transactions));
   }
 
   public void setMatchedFilterIndices(List<Integer> newMatchedFilterIndices) {
-      // Perform input validation
-      if (newMatchedFilterIndices == null) {
-	  throw new IllegalArgumentException("The matched filter indices list must be non-null.");
+    // Perform input validation
+    if (newMatchedFilterIndices == null) {
+      throw new IllegalArgumentException("The matched filter indices list must be non-null.");
+    }
+    for (Integer matchedFilterIndex : newMatchedFilterIndices) {
+      if ((matchedFilterIndex < 0) || (matchedFilterIndex > this.transactions.size() - 1)) {
+        throw new IllegalArgumentException(
+            "Each matched filter index must be between 0 (inclusive) and the number of transactions (exclusive).");
       }
-      for (Integer matchedFilterIndex : newMatchedFilterIndices) {
-	  if ((matchedFilterIndex < 0) || (matchedFilterIndex > this.transactions.size() - 1)) {
-	      throw new IllegalArgumentException("Each matched filter index must be between 0 (inclusive) and the number of transactions (exclusive).");
-	  }
-      }
-      // For encapsulation, copy in the input list 
-      this.matchedFilterIndices.clear();
-      this.matchedFilterIndices.addAll(newMatchedFilterIndices);
+    }
+    // For encapsulation, copy in the input list
+    this.matchedFilterIndices.clear();
+    this.matchedFilterIndices.addAll(newMatchedFilterIndices);
+    stateChanged();
   }
 
   public List<Integer> getMatchedFilterIndices() {
-      // For encapsulation, copy out the output list
-      List<Integer> copyOfMatchedFilterIndices = new ArrayList<Integer>();
-      copyOfMatchedFilterIndices.addAll(this.matchedFilterIndices);
-      return copyOfMatchedFilterIndices;
+    // For encapsulation, copy out the output list
+    List<Integer> copyOfMatchedFilterIndices = new ArrayList<Integer>();
+    copyOfMatchedFilterIndices.addAll(this.matchedFilterIndices);
+    return copyOfMatchedFilterIndices;
   }
 
   /**
@@ -68,31 +75,49 @@ public class ExpenseTrackerModel {
    * @param listener The ExpenseTrackerModelListener to be registered
    * @return If the listener is non-null and not already registered,
    *         returns true. If not, returns false.
-   */   
+   */
   public boolean register(ExpenseTrackerModelListener listener) {
-      // For the Observable class, this is one of the methods.
-      //
-      // TODO
-      return false;
+    // For the Observable class, this is one of the methods.
+    //
+    // TODO
+    if (listener != null && !listeners.contains(listener)) {
+      listeners.add(listener);
+      return true;
+
+    }
+    return false;
+  }
+
+  public boolean unregister(ExpenseTrackerModelListener listener) {
+    if (listener != null && listeners.contains(listener)) {
+      listeners.remove(listener);
+      return true;
+
+    }
+    return false;
   }
 
   public int numberOfListeners() {
-      // For testing, this is one of the methods.
-      //
-      //TODO
-      return 0;
+    // For testing, this is one of the methods.
+    //
+    // TODO
+    return listeners.size();
   }
 
   public boolean containsListener(ExpenseTrackerModelListener listener) {
-      // For testing, this is one of the methods.
-      //
-      //TODO
-      return false;
+    // For testing, this is one of the methods.
+    //
+    // TODO
+    return listeners.contains(listener);
   }
 
   protected void stateChanged() {
-      // For the Observable class, this is one of the methods.
-      //
-      //TODO
+    // For the Observable class, this is one of the methods.
+    //
+    // TODO
+    for (ExpenseTrackerModelListener listener : listeners) {
+      listener.update(this);
+    }
   }
+
 }
--- expense_tracker/test/TestExample.java
+++ expense_tracker/test/TestExample.java
@@ -328,12 +328,12 @@ public class TestExample {
 	// Perform setup and check pre-conditions
 	ExpenseTrackerModel newModel = new ExpenseTrackerModel();
 	ExpenseTrackerView newView = new ExpenseTrackerView();
-	assertNotNull(model);
-	assertEquals(model.numberOfListeners(), 0);
-	assertNotNull(view);
+	assertNotNull(newModel); //change model to newModel
+	assertEquals(newModel.numberOfListeners(), 0);
+	assertNotNull(newView); //change view to newView
 	
 	// Call the unit under test
-        boolean registered = newModel.register(view);
+        boolean registered = newModel.register(newView); //change view to newView
 
 	// Check the post-conditions
 	assertTrue(registered);
